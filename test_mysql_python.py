a7546036e5f48ce89b7f6424b21c56e8cdb80586
Shiv Konar <shiv.konar@outlook.com>
2016-05-31 13:54:55 +0100

    Test mysql installtion for python

---
+++ test_mysql_python.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import MySQLdb as mdb
+import sys
+
+try:
+    con = mdb.connect('localhost', 'lotteryuser', 'lotteryuser', 'lottery');
+
+    cur = con.cursor()
+    cur.execute("SELECT VERSION()")
+
+    ver = cur.fetchone()
+    
+    print "Database version : %s " % ver
+    
+except mdb.Error, e:
+  
+    print "Error %d: %s" % (e.args[0],e.args[1])
+    sys.exit(1)
+    
+finally:    
+        
+    if con:    
+        con.close()
