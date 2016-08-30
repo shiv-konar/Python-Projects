cf2822c9b5b945d0dc320fa3b442426152a7e8c8
shiv-konar <Balajipadmavati1>
2016-08-30 21:28:23 +0100

    Calculate n digits of Pi

---
+++ pi_upto_n_digits.py
@@ -0,0 +1,38 @@
+from decimal import *
+import math, sys
+from math import factorial
+
+
+def calculatePi(k):
+    sum = 0
+    getcontext().prec = k
+    getcontext().rounding = ROUND_FLOOR
+    for i in range(k + 1):
+        up = factorial(6 * i) * (13591409 + 545140134 * i)
+        down = factorial(3 * i) * (factorial(i)) ** 3 * (640320 ** (3 * i))
+        sum += up / down
+    sum = Decimal(sum)
+
+    pi = Decimal(426880 * math.sqrt(10005))/sum
+
+    return pi
+
+
+def main():
+    sys.setrecursionlimit(100)
+    print "PI Calculator \n"
+    print "This program can be used to calculate digits of PI upto kth place(max 100)\n"
+    while True:
+        k = raw_input("How many digits to calculate or quit to exit? ")
+        if k.lower() == "quit":
+            break
+        elif not k.isdigit():
+            print "Please enter a number. Try again"
+        elif int(k) > 100:
+            print "Please enter a number below 100"
+        else:
+            print calculatePi(int(k))
+
+
+if __name__ == '__main__':
+    main()
